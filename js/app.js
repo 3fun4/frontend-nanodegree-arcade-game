7e14a276bf939fcad718b61211bcbf6da8d14705
3fun4 <zz3fun4@gmail.com>
2018-01-23 13:19:42 +0800

    feature basic functions

--- js/app.js
+++ js/app.js
@@ -3,8 +3,6 @@
  *
  */
 
-var X = 101;
-var Y = 83;
 
 var PLAYER_START_POS_X = 202.5;
 var PLAYER_START_POS_Y = 383;
@@ -15,18 +13,6 @@ var PLAYER_MAX_X_POS = 402.5;
 var PLAYER_MAX_Y_POS = 383;
 
 var NUM_ENENIES = 4;
-
-//TODO: //
-var Charactor = function(x,y,speed,sprite) {
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    this.sprite = sprite;
-}
-Charactor.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-
 
 /**
  * Enemy
@@ -82,13 +68,6 @@ var Player = function(x,y,speed) {
     this.sprite = 'images/char-boy.png';
 }
 Player.prototype.update = function() {
-    //check whether the player has reached the top
-    if (player.y <= 0) {
-        player.x = PLAYER_START_POS_X;
-        player.y = PLAYER_START_POS_Y;
-        console.log('you made it!');//Win!!!
-
-    }
 
 }
 Player.prototype.render = function() {
@@ -108,7 +87,7 @@ Player.prototype.handleInput = function(keyPress) {
     if (keyPress == 'down') {
         player.y += player.speed - 20;
     }
-    console.log('keyPress is: ' + keyPress);
+
     //prevent the player from moving out of canvas
     if (player.y > PLAYER_MAX_Y_POS ) {
         player.y = PLAYER_MAX_Y_POS;
@@ -121,6 +100,15 @@ Player.prototype.handleInput = function(keyPress) {
     }
     if (player.x < PLAYER_MIN_X_POS) {
         player.x = PLAYER_MIN_X_POS;
+    }
+    //check whether the player has reached the top
+    if (player.y <= 0) {
+        player.x = PLAYER_START_POS_X;
+        player.y = PLAYER_START_POS_Y;
+        console.log('you made it!');//Win!!!
+        //TODO: //display an animation
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        ctx.globalAlpha = 1;
     }
 }
 Player.prototype.reset = function() {
@@ -139,10 +127,8 @@ Player.prototype.reset = function() {
 var player = new Player(PLAYER_START_POS_X, PLAYER_START_POS_Y, 50);
 var allEnemies = [];
 for (var i=0; i < NUM_ENENIES; i++) {
-    //var startX = Math.floor(Math.random() * 5);
-    var startX = -(Math.floor((Math.random() * 5) + 1) * X);
-    //var startY = Math.random() * 184 + 50;
-    var startY = Math.floor((Math.random() * 3) + 1) * Y;
+    var startX = Math.random() * 100;
+    var startY = Math.random() * 184 + 50;
     var startSpeed = Math.floor((Math.random() * 50) + 50);
     allEnemies.push(new Enemy(startX,startY,startSpeed));
 }
